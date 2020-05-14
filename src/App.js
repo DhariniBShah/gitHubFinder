31d38f137fcd141d4a8d417b227b02348474c3d4
Dharini S <shah.dharin@husky.neu.edu>
2020-05-13 21:05:50 -0400

    aded the initial api requtest - asyn await

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import './App.css';
 
 class App extends Component {
+	state = {
+		users: [],
+		loading: false,
+	};
+	async componentDidMount() {
+		this.setState({ loading: true });
+		const res = axios.get('https://api.github.com/users');
+		this.setState({ users: (await res).data, loading: false });
+	}
 	render() {
+		const { users, loading } = this.state;
 		return (
 			<div className='App'>
 				<Navbar />
 				<div className='container'>
-					<Users />
+					<Users users={users} loading={loading} />
 				</div>
 			</div>
 		);
