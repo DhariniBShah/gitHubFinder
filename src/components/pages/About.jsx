aa28c2b64aa4248215b81a245ddad5c56c0b0061
Dharini S <shah.dharin@husky.neu.edu>
2020-05-14 23:13:25 -0400

    updated the about page

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -3,9 +3,34 @@ const About = () => {
 	return (
 		<div className='container'>
 			<h1>About this App</h1>
-			<p>App to Search Github Users</p>
-			<p>Version 1.0.0</p>
-			<p> - Created By Dharini Shah</p>
+			<p>A simple webapp app to Search Github Users</p>
+			<p className='text-primary'>Version 1.0.1</p>
+			<br />
+			<p>
+				<h2>Demonstrating the use of ~</h2>
+				<ul>
+					<li>
+						React Hooks (useState, useEffect, useContext, useReducer, useRef) in real
+						projects
+					</li>
+					<li>Context API & App Level State</li>
+					<li>Working with Api's (async) using axios</li>
+					<li>React Fundamentals (Components, props, state, router)</li>
+				</ul>
+			</p>
+			<br />
+			<p>
+				<h2>Regarding previous version</h2>
+				v1.0.0 used Old School React Fundamentals, functional and class level
+				components to create this app! it was later refactored to use React Hooks
+				and Context Api to comply with industry standards
+			</p>
+			<br />
+			<p>
+				<strong>
+					<em>- Created By Dharini Shah</em>
+				</strong>
+			</p>
 		</div>
 	);
 };
